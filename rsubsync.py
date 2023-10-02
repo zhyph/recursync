d5ec3388501a6fb8f687d434b8c5289bd3372054
Artur <artur.almeida1@outlook.com>
2023-10-02 16:18:52 -0300

    [add] args reads multiple langs, overwrite 'old' srt file instead of creating a temp/old one, use a local file to keep track of already processed files (idea taken from filebot) and accept interrupting script with ctrl+c

--- rsubsync.py
+++ rsubsync.py
@@ -2,29 +2,41 @@ import fnmatch
 import os
 import subprocess
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--lang',
-                    dest='language_code',
-                    required=True,
-                    help="Specify a single language code of the subtitles you want to sync (ie. 'es', 'en'...)")
+parser.add_argument(
+    "--lang",
+    dest="language_codes",
+    required=True,
+    nargs="+",
+    help="Specify one or more language codes of the subtitles you want to sync (ie. es en...)",
+)
 
-parser.add_argument('--path',
-                    dest='path',
-                    default='.',
-                    required=False,
-                    help="Specify the path to sync. It will be '.' by default (current directory)")
+parser.add_argument(
+    "--path",
+    dest="path",
+    default=".",
+    required=False,
+    help="Specify the path to sync. It will be '.' by default (current directory)",
+)
 
 args = parser.parse_args()
 
 SETTINGS = {
-    "SUBSYNC_PATH": 'subsync',
+    "SUBSYNC_PATH": "subsync",
     "PATH_TO_SYNC": args.path,
-    "VIDEO_EXTENSIONS": ['.mkv', '.mp4', '.avi'],
-    "SUBTITLE_EXTENSION_TO_SYNC": f'.{args.language_code}.srt',
-    "TEMP_OUTPUT_NAME": 'output.srt',
-    "OLD_SUFFIX": '.old',
-    "FAILED_SUFFIX": '.failed'
+    "VIDEO_EXTENSIONS": [".mkv", ".mp4", ".avi"],
+    "FAILED_SUFFIX": ".failed",
+    "USED_SUBS_FILE": os.path.join(
+        os.path.dirname(
+            os.path.realpath(__file__)
+        ),  # To avoid using the current directory where the script was executed
+        "used-subs.txt",
+    ),
+    "SUBTITLE_EXTENSION_TO_SYNC": [
+        f".{lang_code}.srt" for lang_code in args.language_codes
+    ],
 }
 
 
@@ -32,13 +44,13 @@ def get_subtitle(root, filename):
     return os.path.join(root, filename)
 
 
-def get_file_base(root, filename):
-    return os.path.join(root, filename.split(SETTINGS['SUBTITLE_EXTENSION_TO_SYNC'])[0])
+def get_file_base(root, filename, extension):
+    return os.path.join(root, filename.split(extension)[0])
 
 
 def get_matching_video_extension(filename):
     video_match = None
-    for extension in SETTINGS['VIDEO_EXTENSIONS']:
+    for extension in SETTINGS["VIDEO_EXTENSIONS"]:
         video_file_to_check = f"{filename}{extension}"
         if os.path.exists(video_file_to_check):
             video_match = video_file_to_check
@@ -46,51 +58,68 @@ def get_matching_video_extension(filename):
     return video_match
 
 
-def execute_subsync_process(video, subtitle, output_subtitle, output_file):
-    return subprocess.call([SETTINGS['SUBSYNC_PATH'], video, "-i", subtitle, "-o", output_subtitle], stderr=output_file)
+def execute_subsync_process(video, subtitle):
+    return subprocess.call(
+        [SETTINGS["SUBSYNC_PATH"], video, "-i", subtitle, "--overwrite-input"]
+    )
 
 
-def replace_subtitle(subtitle, output):
-    os.rename(subtitle,
-              f"{subtitle}{SETTINGS['OLD_SUFFIX']}")
-    os.rename(output, subtitle)
-    os.remove(
-        f"{subtitle}{SETTINGS['FAILED_SUFFIX']}")
+def read_used_subs_file():
+    used_subs = set()
+    if os.path.exists(SETTINGS["USED_SUBS_FILE"]):
+        with open(SETTINGS["USED_SUBS_FILE"], "r") as f:
+            used_subs = set(f.read().splitlines())
+    else:
+        open(SETTINGS["USED_SUBS_FILE"], "w").close()
+    return used_subs
+
+
+def write_used_subs_file(filename):
+    with open(SETTINGS["USED_SUBS_FILE"], "a") as f:
+        f.write(filename + "\n")
 
 
 def main():
-    if os.path.exists(SETTINGS['PATH_TO_SYNC']):
-        print(f"Starting sync process in path: {SETTINGS['PATH_TO_SYNC']}...")
+    print(f"Starting sync process in path: {SETTINGS['PATH_TO_SYNC']}...")
+    used_subs = read_used_subs_file()
+    try:
         for root, dirnames, filenames in os.walk(f"{SETTINGS['PATH_TO_SYNC']}"):
-            for filename in fnmatch.filter(filenames, f"*{SETTINGS['SUBTITLE_EXTENSION_TO_SYNC']}"):
-                subtitle = get_subtitle(root, filename)
-                file_base = get_file_base(root, filename)
-                video_match = get_matching_video_extension(file_base)
+            for lang_code in SETTINGS["SUBTITLE_EXTENSION_TO_SYNC"]:
+                for filename in fnmatch.filter(filenames, f"*{lang_code}"):
+                    if filename in used_subs:
+                        continue
+                    subtitle = get_subtitle(root, filename)
+                    file_base = get_file_base(root, filename, lang_code)
+                    video_match = get_matching_video_extension(file_base)
 
-                if video_match is not None:
-                    if os.path.exists(f"{subtitle}{SETTINGS['OLD_SUFFIX']}"):
-                        print("Sub already synced")
-                    elif os.path.exists(f"{subtitle}{SETTINGS['FAILED_SUFFIX']}"):
-                        print("This video failed previously, won't try again")
-                    else:
+                    if video_match is not None:
                         try:
                             print(f"Starting sync process of: {filename}")
-                            output_filename = os.path.join(
-                                root, SETTINGS['TEMP_OUTPUT_NAME'], )
-                            failed_file = f"{subtitle}{SETTINGS['FAILED_SUFFIX']}"
-                            with open(failed_file, "w+") as outfile:
-                                process_code = execute_subsync_process(video_match, subtitle,
-                                                                       output_filename, outfile)
+                            write_used_subs_file(filename)
+                            used_subs = read_used_subs_file()
+
+                            process_code = execute_subsync_process(
+                                video_match, subtitle
+                            )
+
                             if process_code != 0:
-                                raise Exception
-                            replace_subtitle(subtitle, output_filename)
-                        except:
+                                raise subprocess.CalledProcessError(
+                                    process_code, "subsync"
+                                )
+                        except subprocess.CalledProcessError as e:
                             print(
-                                f"There has been an error in the syncing process of: ${filename}")
-                else:
-                    print(f"There's no video file for: {filename}")
+                                f"There has been an error in the syncing process of: {filename}"
+                            )
+                            print(f"Error message: {e}")
+                    else:
+                        print(f"There's no video file for: {filename}")
+    except KeyboardInterrupt:
+        print("Exiting application...")
+        exit()
+
+
+if __name__ == "__main__":
+    if os.path.exists(SETTINGS["PATH_TO_SYNC"]):
+        main()
     else:
         print("Specified path does not exist")
-
-
-main()
